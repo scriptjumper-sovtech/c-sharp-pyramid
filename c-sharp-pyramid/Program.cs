e786bfdf5b4565c53f2a2911363716a997dc9b48
Shaeen Kumalaseelan <98379265+scriptjumper-sovtech@users.noreply.github.com>
2023-01-15 21:16:47 +0200

    first commit

---
+++ c-sharp-pyramid/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter total number of layers for pyramid: ");
+string layers = Console.ReadLine();
+
+if (layers != "")
+{
+    int numberOfLayers = 6;//Int32.Parse(layers);
+    int space = 0;
+    int number = 0;
+
+    Console.WriteLine("Printing pyramid");
+    for (int i = 1; i <= numberOfLayers; i++) // Total number of layer for pyramid  
+    {
+        // for (space = 1; space <= (numberOfLayers - i); space++) // Loop For Space  
+        //     Console.Write(" ");  
+        for (number = 1; number <= i; number++) //increase the value  
+            Console.Write('*');  
+        // for (number = (i - 1); number >= 1; number--) //decrease the value  
+        //     Console.Write('*');
+        // Console.WriteLine();
+    }
+}
+else
+{
+    Console.WriteLine("Can not display pyramid!");
+}
